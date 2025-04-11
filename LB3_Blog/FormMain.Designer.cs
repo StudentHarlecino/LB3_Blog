c38129cc96023976856a4e28f8348b0ffab4e56b
Aleksandra Sycheva <sashasychevaisio@gmail.com>
2025-04-11 08:49:33 +0500

    исправление ошибок

---
+++ LB3_Blog/FormMain.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace LB3_Blog
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridViewUsers = new DataGridView();
+            buttonSave = new Button();
+            panelTop = new Panel();
+            panelLeft = new Panel();
+            panelRight = new Panel();
+            dataGridViewPosts = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewUsers).BeginInit();
+            panelTop.SuspendLayout();
+            panelLeft.SuspendLayout();
+            panelRight.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewPosts).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewUsers
+            // 
+            dataGridViewUsers.BackgroundColor = Color.White;
+            dataGridViewUsers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewUsers.Dock = DockStyle.Fill;
+            dataGridViewUsers.Location = new Point(10, 10);
+            dataGridViewUsers.Margin = new Padding(4);
+            dataGridViewUsers.Name = "dataGridViewUsers";
+            dataGridViewUsers.Size = new Size(507, 540);
+            dataGridViewUsers.TabIndex = 0;
+            dataGridViewUsers.SelectionChanged += dataGridViewUsers_SelectionChanged;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Dock = DockStyle.Left;
+            buttonSave.Location = new Point(10, 10);
+            buttonSave.Margin = new Padding(4);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(96, 50);
+            buttonSave.TabIndex = 1;
+            buttonSave.Text = "Сохранить";
+            buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += this.buttonSave_Click;
+            // 
+            // panelTop
+            // 
+            panelTop.Controls.Add(buttonSave);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(0, 0);
+            panelTop.Margin = new Padding(4);
+            panelTop.Name = "panelTop";
+            panelTop.Padding = new Padding(10);
+            panelTop.Size = new Size(1029, 70);
+            panelTop.TabIndex = 2;
+            // 
+            // panelLeft
+            // 
+            panelLeft.Controls.Add(dataGridViewUsers);
+            panelLeft.Dock = DockStyle.Left;
+            panelLeft.Location = new Point(0, 70);
+            panelLeft.Name = "panelLeft";
+            panelLeft.Padding = new Padding(10);
+            panelLeft.Size = new Size(527, 560);
+            panelLeft.TabIndex = 3;
+            // 
+            // panelRight
+            // 
+            panelRight.Controls.Add(dataGridViewPosts);
+            panelRight.Dock = DockStyle.Right;
+            panelRight.Location = new Point(524, 70);
+            panelRight.Name = "panelRight";
+            panelRight.Padding = new Padding(10);
+            panelRight.Size = new Size(505, 560);
+            panelRight.TabIndex = 4;
+            // 
+            // dataGridViewPosts
+            // 
+            dataGridViewPosts.BackgroundColor = Color.White;
+            dataGridViewPosts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewPosts.Dock = DockStyle.Fill;
+            dataGridViewPosts.Location = new Point(10, 10);
+            dataGridViewPosts.Margin = new Padding(4);
+            dataGridViewPosts.Name = "dataGridViewPosts";
+            dataGridViewPosts.Size = new Size(485, 540);
+            dataGridViewPosts.TabIndex = 1;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1029, 630);
+            Controls.Add(panelRight);
+            Controls.Add(panelLeft);
+            Controls.Add(panelTop);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(4);
+            Name = "FormMain";
+            Text = "Пользователи";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewUsers).EndInit();
+            panelTop.ResumeLayout(false);
+            panelLeft.ResumeLayout(false);
+            panelRight.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewPosts).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewUsers;
+        private Button buttonSave;
+        private Panel panelTop;
+        private Panel panelLeft;
+        private Panel panelRight;
+        private DataGridView dataGridViewPosts;
+    }
+}
---
+++ LB3_Blog/FormMain.cs
@@ -0,0 +1,55 @@
+
+using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace LB3_Blog
+{
+    public partial class FormMain : Form
+    {
+        private BlogContext? db;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            this.db = new BlogContext();
+
+            this.db.Users.Load();
+            this.dataGridViewUsers.DataSource = db.Users.Local.ToBindingList();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            this.db?.Dispose();
+            this.db = null;
+        }
+
+        private void dataGridViewUsers_SelectionChanged(object sender, EventArgs e)
+        {
+            if (this.db != null)
+            {
+                var user = (User)this.dataGridViewUsers.CurrentRow.DataBoundItem;
+
+                if (user != null)
+                {
+                    this.db.Entry(user).Collection(e => e.Posts).Load();
+                    this.dataGridViewPosts.DataSource = user.Posts;
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            this.db!.SaveChanges();
+
+            this.dataGridViewUsers.Refresh();
+            this.dataGridViewPosts.Refresh();
+        }
+    }
+}
